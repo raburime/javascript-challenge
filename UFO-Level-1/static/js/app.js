3594fe3f59c9fd177d86b18f01fef49987d080a3
Rachel <rachelaburime@gmail.com>
2021-04-15 15:59:31 -0500

    Level I

---
+++ UFO-Level-1/static/js/app.js
@@ -0,0 +1,19 @@
+showData(data);
+
+function showData(data) {
+    d3.select('tbody').html('');
+    data.forEach(obj => {
+        var row = d3.select('tbody').append('tr');
+        Object.values(obj).forEach(val=>row.append('td').text(val));
+    });
+};
+
+d3.select('button').on('click',handleClick);
+
+function handleClick() {
+    var selection = d3.select('input').node().value;
+    console.log(selection);
+
+    var filteredData = data.filter(obj => obj.datetime == selection);
+    showData(filteredData);
+};
